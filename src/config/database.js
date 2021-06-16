f6577587132c369f59fdb4b28b286f65a2cd4811
Leonardo-PRT <leonardoporto51@gmail.com>
2021-06-15 23:00:08 -0300

    configuring database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: '*****',
+    password: '*******',
+    database: 'BrightCities',
+    port: 3306,
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
---
+++ src/database/migrations/20210616010644-todoList.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('todoList', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
