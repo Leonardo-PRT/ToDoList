9ac9e373a534aea537f51b096e9beab933ab60b7
Leonardo-PRT <leonardoporto51@gmail.com>
2021-06-18 18:41:26 -0300

    CRUD

--- src/controllers/TodoListController.js
+++ src/controllers/TodoListController.js
@@ -2,9 +2,80 @@ const Todo_list = require('../models/Todo_list')
 
 module.exports = {
     async createTask(req, res) {
-        const completed = true
         const {name} = req.body
-        const task = await Todo_list.create({name, completed})
+        const task = await Todo_list.create({ name })
+
+        return res.json(task)
+    },
+
+    async deleteTask(req, res) {
+        const { id } = req.params
+        
+        const task = await Todo_list.findByPk(id)
+
+        if(!task) return res.status(404).json({erro: 'Task not found! '})
+
+        await Todo_list.destroy({
+            where: {
+                id: task.id
+            }
+        })
+
+        return res.json(task)
+    },
+
+    async updateTask(req, res){
+        const { id } = req.params
+        const { name, completed } = req.body
+        const task = await Todo_list.update({name: name, completed: completed},
+            {
+                where: { id: id }
+            })
+        return res.json(task)
+        
+        
+    },
+
+    async listAll(req, res) {
+        const task = await Todo_list.findAll()
+        const not_checked = await Todo_list.findAll({ where: { completed: false}})
+        console.log("-------------------------->" + not_checked.length +" item lefts<-------------------------------")
+        
+        if(!task) return res.status(404).json({erro: 'Task not found! '})
+
+        return res.json(task)
+    },
+
+    async listCompleted(req, res) {
+        const task = await Todo_list.findAll({
+            where: {
+                completed: true
+            }
+        })
+        if(!task) return res.status(404).json({erro: 'Task not found! '})
+
+        return res.json(task)
+
+    },
+    
+    async listActive(req, res) {
+        const task = await Todo_list.findAll({
+            where: {
+                completed: false
+            }
+        })
+        if(!task) return res.status(404).json({erro: 'Task not found! '})
+
+        return res.json(task)
+
+    },
+
+    async deleteCompleted(req, res) {
+        const task = await Todo_list.destroy({
+            where: {
+                completed: true
+            }
+        })
 
         return res.json(task)
     }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const routes = express.Router()
 
-const TodoListController = require('./controllers/TodoListController') 
+const TodoListController = require('./controllers/TodoListController')
 
 routes.get('/', (req, res) => {
+    
     return res.json('Hello World!!')
 })
 
 
 routes.post('/todo_list', TodoListController.createTask)
+routes.post('/completed', TodoListController.listCompleted)
+routes.post('/active', TodoListController.listActive)
+routes.post('/', TodoListController.listAll)
+
+routes.delete('/delete/:id', TodoListController.deleteTask)
+routes.delete('/', TodoListController.deleteCompleted)
+
+
+routes.put('/:id', TodoListController.updateTask)
+
+
 
 
 
