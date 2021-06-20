2fe1a02f92e4fb1dac4c0328e642ca1c667ac7c4
Leonardo-PRT <leonardoporto51@gmail.com>
2021-06-20 13:29:50 -0300

    ajusting server

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const routes = require('./routes')
+const path = require('path')
+
 
 require('./database/index')
 
 const app =  express()
 
+app.use(express.static("public"))
 app.use(express.json())
 app.use(routes)
 app.set('view engine', 'html')
 
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html')
 })
 
-app.listen(3333, () => {
+app.listen(3000, () => {
     console.log('Executando o servidor na porta: 3333')
 })
